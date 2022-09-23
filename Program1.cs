d98ef0e8874a4b21322d0bc11b494ec4da83df7a
Dmitry <111520046+Little-Pink-Dragon@users.noreply.github.com>
2022-09-23 22:28:38 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());;
+int[] numbers = new int[size];
+int count = 0;
+for (int i = 0; i < size; i++) 
+{
+   Console.Write("Введите число: ");
+numbers[i] = Convert.ToInt32(Console.ReadLine());;
+if (numbers[i] > 0)
+count++;
+}
+PrintArray(numbers);
+void PrintArray(int[] numbers)
+{
+    Console.Write("[");
+     for(int i = 0; i < numbers.Length; i++)
+     { if (i < numbers.Length - 1)
+     {
+     Console.Write(numbers[i] + ", ");
+     }
+     else Console.Write(numbers[i] + "");
+     }
+    Console.Write("]");
+    Console.Write(" -> " + count + " - чисел больше 0");
+}
---
+++ Program2.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b1 - b2)/(k2 - k1);
+double x1 = Math.Round (x, 2 );
+double y = k1 * x + b1;
+double y2 = Math.Round (y, 2 );
+
+Console.WriteLine($"Точки пересечения двух прямых: ({x1}, {y2})");
